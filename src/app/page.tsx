564cf4cc2de860303de9db29238fad271bac52a9
DIVINE <divinetimothyeiq454@gmail.com>
2025-06-13 11:33:25 +0100

    fix: correct syntax and improve loading screen styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // components/LoadingScreen.tsx
 
-"use client"
+"use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
@@ -13,13 +13,12 @@ const LoadingScreen: React.FC = () => {
     return () => clearTimeout(timer); // Cleanup on unmount
   }, []);
 
-
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white text-black overflow-hidden">
       <div className="flex flex-col items-center justify-center h-screen bg-white ">
         <div className="relative">
           {/* Ground Shadow */}
-          <div className="absolute -bottom-3 left-1/2 -translate-x-1/2 w-40 h-10 bg-black/50 rounded-[100%] blur-xl z-0 fou-nd2" />
+          <div className="absolute animate-bounce -bottom-3 left-1/2 -translate-x-1/2 w-40 h-10 bg-black/30 rounded-[50%] blur-xl z-0 fou-nd2" />
 
           {/* Image */}
           <div className="relative z-10">
@@ -28,7 +27,7 @@ const LoadingScreen: React.FC = () => {
               alt="Foundich Logo"
               width={400}
               height={400}
-              className="rounded-2xl shadow-[0px_2px_10px_rgba(0,0,0,0.2)] scale-[0.5] transition-transform py-20 px-10 fou-nd"
+              className="rounded-[50px] shadow-[0px_2px_10px_rgba(0,0,0,0.2)] scale-[0.5] transition-transform py-20 px-10 fou-nd"
             />
           </div>
         </div>
